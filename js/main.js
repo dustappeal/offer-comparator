f602da0028143a8841e1271aea7831e058ca3b55
Dust Appeal <dustappeal@gmail.com>
2016-10-03 03:05:34 +0000

    Implement getStockComp with test, also cleanup some log statements etc.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 function getStockComp(stock, price, vest) {
+	var totalValue = stock * price;
+	var vestValues = [];
+	vest.forEach(function(item) {
+		vestValues.push(item * totalValue / 100);
+	})
+	return vestValues;
 };
 
 function validateVest(vest) {
@@ -7,17 +13,15 @@ function validateVest(vest) {
 	if (strings.length < 1) {
 		throw "Stock vest percentages should be of the format year1/year2/year3"
 	}
-	
+
 	var totalPercent = 0;
-	console.log(strings);
 	strings.forEach(function(item) {
 		var value = parseInt(item);
-		console.log(item)
-		console.log(value)
 		if (value > 0) {
 			percents.push(value);
 			totalPercent += value
-		} else {
+		}
+		else {
 			throw "Invalid vest percentage: " + item;
 		}
 	});
@@ -26,16 +30,18 @@ function validateVest(vest) {
 	}
 	return percents;
 }
-function doCalculation() {
-    var name0 = document.getElementById("name0").value;
-    var salary0 = document.getElementById("salary0").value;
-    var relocation0 = document.getElementById("relocation0").value;
-    var bonus0 = document.getElementById("bonus0").value;
-    var stock0 = document.getElementById("stock0").value;
-    var price0 = document.getElementById("price0").value;
-    var vest0 = document.getElementById("vest0").value;
 
-    document.getElementById('results').innerHTML = "answer is:" + name0;
-    return false;
+function doCalculation() {
+	var name0 = document.getElementById("name0").value;
+	var salary0 = document.getElementById("salary0").value;
+	var relocation0 = document.getElementById("relocation0").value;
+	var bonus0 = document.getElementById("bonus0").value;
+	var stock0 = document.getElementById("stock0").value;
+	var price0 = document.getElementById("price0").value;
+	var vest0 = document.getElementById("vest0").value;
+
+	document.getElementById('results').innerHTML = "answer is:" + name0;
+	return false;
 };
 module.exports.validateVest = validateVest;
+module.exports.getStockComp = getStockComp;
